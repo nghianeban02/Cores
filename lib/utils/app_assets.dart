8ae6fedc32477387f67f0d68a39ce025088c5af1
Huu Nghia <85358691+nghianeban02@users.noreply.github.com>
2024-11-14 15:01:46 +0700

    first commit

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,20 @@
+class AppAssets {
+  static const String logo = 'assets/image/logo.png';
+  static const String brLogin = 'assets/image/brLogin.png';
+  static const String brRegister = 'assets/image/brRegister.png';
+  static const String brWelcome = 'assets/image/brWelcome.png';
+  static const String brHome = 'assets/image/brHome.jpg';
+  static const String slide1 = 'assets/image/slide1.jpg';
+  static const String slide2 = 'assets/image/slide2.jpg';
+  static const String slide3 = 'assets/image/slide3.jpg';
+  static const String slide4 = 'assets/image/slide4.jpg';
+  static const String slide5 = 'assets/image/slide5.jpg';
+  static const String splash = 'assets/image/splashscreen.png';
+  static const String fingerPrint = 'assets/image/fingerPrint.png';
+  static const String faceId = 'assets/image/faceId.png';
+  static const String br_button = 'assets/image/br_button.png';
+  static const String user_login = 'assets/image/user_login.png';
+  static const String logo_login = 'assets/image/logo_login.png';
+  static const String adv_login = 'assets/image/adv-login.png';
+  static const String br_login = 'assets/image/br_login.png';
+}
